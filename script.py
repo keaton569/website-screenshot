afef8d76cc41fecb21eed5835857889c330bdb72
keaton569 <keatonguitar123@gmail.com>
2019-10-11 12:31:48 -0400

    Update script.py

--- script.py
+++ script.py
@@ -3,7 +3,14 @@ import os
 import csv
 import time
 
-with open('fullList.csv') as csv_file:
+##USES THIS URL SCHEME INSIDE THE CSV FILE ---- IN THIS CASE COLUMN 4
+##https://image.thum.io/get/width/300/noanimate/wait/3/[YOUR_URL]
+
+
+##HEADERS IN CSV CHANGE WITH EACH GEO CAUSE IM LAZY
+#################################################################
+
+with open('US.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
@@ -12,13 +19,9 @@ with open('fullList.csv') as csv_file:
             line_count += 1
         else:
             try:
-                urllib.request.urlretrieve(f'{row[6]}', f'{row[5]}.jpg')
+                urllib.request.urlretrieve(f'{row[4]}', f'{row[0]}.jpg')
                 line_count += 1
-                
-                #wait 7 secs so we dont overload the server
-                #slow sites will produce a thum.io image instead of the website thumbnail
-                time.sleep(7)
-                
             except urllib.error.HTTPError as e:
-                print("error: ", e)
+                print(f'{row[0]}: '"error: ", e)
     print(f'Processed {line_count} lines.')
+
